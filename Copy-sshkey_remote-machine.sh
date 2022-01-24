3bbf195971e1ea0f3dd6e3a64978ba40f542aa45
STsariuk <37350526+STsariuk@users.noreply.github.com>
2022-01-24 11:07:02 +0200

    Update Copy-sshkey_remote-machine.sh

--- Copy-sshkey_remote-machine.sh
+++ Copy-sshkey_remote-machine.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 PUBLIC="ssh-rsa ..."
 echo $PUBLIC
-echo $PUBLIC >> test
-scp test lg:~/.ssh/test1
-#copy $PUBLIC KEY to remote machine in file
+echo $PUBLIC >> <the name of the file where the key will be written> 
+scp <the name of the file where the key will be written> <remote host>:~/.ssh/authorized_keys
